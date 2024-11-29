b01d09904cfb20c81d650748d6f4250755eff644
Petruches <petya.bykov.99@mail.ru>
2024-11-29 19:23:58 +0300

    add func

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-#!/usr/bin/python3.11
+#!/usr/bin/env python3
+
 TEXT_SESSION: str = "ERROR"
 file = open("./log.log", 'r')
+
+
+def send_telegram(text: str):
+    import requests
+    token = "7939669010:AAEG8-KmNSVoisckbRFYM36Kq2-HkXJZWrU"
+    url = "https://api.telegram.org/bot"
+    channel_id = "-1002356269285"
+    url += token
+    method = url + "/sendMessage"
+    r = requests.post(method, data={
+         "chat_id": channel_id,
+         "text": text
+          })
+    if r.status_code != 200:
+        raise Exception("post_text error")
+
+send_telegram("asdasdasdasd")
 
 def start():
     print("start")
@@ -11,4 +29,4 @@ def start():
         elif TEXT_SESSION in chunk:
             print(chunk)
 
-start()
+# start()
