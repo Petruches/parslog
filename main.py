d4e3a1e46db5c3ee8dbd7be86c268792662952bb
Petruches <petya.bykov.99@mail.ru>
2024-11-29 20:33:10 +0300

    fix

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ FILE = open("./log.log", 'r')
 
 
 def send_telegram(text: str):
-    import requests
-    token = "7939669010:AAEG8-KmNSVoisckbRFYM36Kq2-HkXJZWrU"
-    url = "https://api.telegram.org/bot"
-    channel_id = "-1002356269285"
-    url += token
-    method = url + "/sendMessage"
-    r = requests.post(method, data={
-         "chat_id": channel_id,
-         "text": text
-          })
-    if r.status_code != 200:
-        raise Exception("post_text error")
+    try:
+        import requests
+        token = "7939669010:AAEG8-KmNSVoisckbRFYM36Kq2-HkXJZWrU"
+        url = "https://api.telegram.org/bot"
+        channel_id = "-1002356269285"
+        url += token
+        method = url + "/sendMessage"
+        r = requests.post(method, data={
+            "chat_id": channel_id,
+            "text": text
+        })
+        #if r.status_code != 200:
+        #    raise Exception("post_text error")
+    except Exception as e:
+        print(e)
+
 
 #send_telegram("asdasdasdasd")
 
@@ -29,7 +33,7 @@ def start():
             if not chunk:
                 continue
             elif TEXT_SESSION in chunk:
-                send_telegram(chunk)
+                send_telegram(str(chunk))
                 print(chunk)
     except Exception as e:
         print(e)
